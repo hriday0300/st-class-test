0067c63337e442ebc7c1fc33395c8e91ae67830b
hriday0300 <78411309+hriday0300@users.noreply.github.com>
2022-08-26 10:41:36 +0530

    ques2.cpp

---
+++ ques2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Function to find the repeating
+// elements with there count
+map<int,int> findRepeating(int arr[], int size){
+ 
+    // Hash map to store the
+    // frequency of elements
+    map<int,int> frequency;
+     
+    // Loop to store the frequency of
+    // elements of array
+    for (int i = 0; i < size; i++)
+    frequency[arr[i]]++;
+    return frequency;
+}
+         
+// Driver Code
+int main(){
+    int arr[] = {4, 4, 5, 5, 6};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    map<int,int> frequency = findRepeating(arr, arr_size);
+    cout<<"Below is the frequency of repeated elements -"<<endl;
+    for(auto x : frequency){
+        if (frequency[x.first] > 1)
+            cout<<x.first<<" --> "<<frequency[x.first]<<endl;
+    }
+}
